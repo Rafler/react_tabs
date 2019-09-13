12785e3dd4046d802d08a9b3e8d3338a41f8abab
Rafler <amuronyk@gmail.com>
2019-09-13 10:30:47 +0300

    Updates

---
+++ precache-manifest.296da57a9f0e771c4d5a1a79f74ed88c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf217aa19c5674a69f639974dc81e597",
+    "url": "/react_tabs/index.html"
+  },
+  {
+    "revision": "14ad0a00b7c083139154",
+    "url": "/react_tabs/static/css/main.b0907ce2.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/react_tabs/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "14ad0a00b7c083139154",
+    "url": "/react_tabs/static/js/main.54e514ee.chunk.js"
+  },
+  {
+    "revision": "010ecae327fc4474afd9",
+    "url": "/react_tabs/static/js/runtime~main.310f5775.js"
+  }
+]);
